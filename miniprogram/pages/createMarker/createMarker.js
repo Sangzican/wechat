8634ab7beed28d73b86c20c08bd2a7101067fe9f
sangzican <1642440601@qq.com>
2020-09-18 17:04:28 +0800

    2333

--- miniprogram/pages/createMarker/createMarker.js
+++ miniprogram/pages/createMarker/createMarker.js
@@ -47,6 +47,7 @@ Page({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          //23333
           wx.getUserInfo({
             success: res => {
               this.setData({
